7eb97bf5cafd4edc870eab4e9108154be0a8b6a7
kevin-itdev <128466750+kevin-itdev@users.noreply.github.com>
2025-01-18 21:30:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const app = express();
 app.get("/", (req, res) => { res.send("Express on Vercel"); }); 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+module.exports = app; // Export the Express app
