49a9bc2fc6b60213fc79f8235f143bea2d76c6af
kevin-itdev <128466750+kevin-itdev@users.noreply.github.com>
2025-01-18 21:06:37 +0900

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-
-    if(req.body.path != undefined)
-        { path = req.body.path; console.log(path); }   
-
-    console.log('Request received:', req.method);
-    res.status(200).json({ message: 'Hello, world!' });
-}
